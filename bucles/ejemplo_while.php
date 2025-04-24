3cf70d0ebae1a7ce051eb318314a557ade4bb6e3
duque <duque@DESKTOP-P04FB6I>
2025-04-23 23:46:02 -0500

    bucle_while

---
+++ bucles/ejemplo_while.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $var=1;
+    $contador=0;
+
+    while($var<6){
+        echo  "$var número";
+        $var++;
+    }
+
+    echo "Fin";
+
+    ?>
+</body>
+</html>
