8dc0c0475c729295253588038768f03e4d83b916
duque <duque@DESKTOP-P04FB6I>
2025-04-24 23:32:55 -0500

    ejecucción de bucle do while

---
+++ bucles/ejemplo_do_while.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $var=7;
+
+    do{
+        echo  "Estamos ejecutando el bucle.<br>";
+        $var++;
+    }while($var<6);
+
+    echo "Fin";
+
+    ?>
+</body>
+</html>
