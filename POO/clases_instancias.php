8e5a7ec9431d2f4875b260f2e318caa256d2eb34
duque <duque@DESKTOP-P04FB6I>
2025-05-05 00:21:38 -0500

    clases,funciones, metodos, instancias, constructores...

--- POO/clases_instancias.php
+++ POO/clases_instancias.php
@@ -1,26 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<?php
+    <?php
 
-class Coche{
+    class Coche
+    {
 
+        var $ruedas;
+        var $color;
+        var $motor;
 
-}
+        function __construct()
+        { // Constructor moderno
+            $this->ruedas = 4;
+            $this->color = "";
+            $this->motor = "1600"; // Corrección aquí
+        }
 
-$renault=new Coche();
-$mazda=new Coche();
-$seat=new Coche();
+        function arrancar()
+        {
+            echo "Estoy arrancando<br>";
+        }
 
+        function girar()
+        {
+            echo "Estoy girando<br>";
+        }
 
-?>
+        function frenar()
+        {
+            echo "Estoy frenando<br>";
+        }
+    }
 
+    $renault = new Coche();
+    $mazda = new Coche();
+    $seat = new Coche();
 
-
+    $mazda->girar();
+    echo $mazda->ruedas;
+    ?>
 </body>
+
 </html>
